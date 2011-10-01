dc15da7d64efc2edb13431313c24c24b41e9c3db
BaBaBags <ryan@ryanbagwell.com>
2011-10-01 14:35:44 -0500

    removing max number of records for export

--- wordpress-weddings.php
+++ wordpress-weddings.php
@@ -355,7 +355,7 @@ class WPWeddings {
         $csv = '"Name","Address 1","Address 2","City","State","ZIP","Email"'."\n";
         
         //get all wedding parties
-        $parties = get_posts(array('post_type'=>'wedding_guests'));
+        $parties = query_posts(array('post_type'=>'wedding_guests'));
         
         //assign the post meta to each party
         foreach($parties as $party) {
